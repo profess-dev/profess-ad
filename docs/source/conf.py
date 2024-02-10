4a3c12d9381bdde6f200e64483c6fecfbec829f1
CW Tan <chuinwei.tan@gmail.com>
2024-02-10 01:55:07 -0500

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,11 +22,11 @@ sys.path.insert(0, os.path.abspath('../../professad'))
 # -- Project information -----------------------------------------------------
 
 project = 'PROFESS-AD'
-copyright = '2022, Chuin Wei Tan'
+copyright = '2024, Chuin Wei Tan'
 author = 'Chuin Wei Tan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
