03a8706207c98cbf1a2a5369f544b0622df7771c
CW Tan <chuinwei.tan@gmail.com>
2024-02-10 23:41:00 -0500

    update for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../professad'))
+sys.path.insert(0, os.path.abspath('../../src/professad'))
 
 
 # -- Project information -----------------------------------------------------
